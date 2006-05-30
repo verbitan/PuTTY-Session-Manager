e859d992db2dee0b9bbff1df3f0020e253748cc6
david <david@024b630a-0915-0410-a176-cfe2cd84e366>
2006-05-30 22:57:15 +0000

    Addition of resources and settings
    git-svn-id: svn://svn.code.sf.net/p/puttysm/code/trunk@3 024b630a-0915-0410-a176-cfe2cd84e366

---
+++ WindowsApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PuTTY Session Manager")]
+[assembly: AssemblyDescription("Organise your PuTTY ( © Simon Tatham ) sessions into folders.\r\n" 
+    + "Inspired by QuickPuTTY by Olivier Deckmyn, re-written in C#.\r\n\r\n" 
+    + "This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License " 
+    + "as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\r\n\r\n"
+    + "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of "
+    + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\r\n\r\n"
+    + "You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software "
+    + "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PuTTY Session Manager")]
+[assembly: AssemblyCopyright("Copyright © David Riseley 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43127c28-7e27-4abf-940c-855f5737f95e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
