6ec4fce874d408c617e79b400006c80458aae360
dmrpsm <dmrpsm@024b630a-0915-0410-a176-cfe2cd84e366>
2007-06-24 10:33:17 +0000

    Missed a file for implementing settings upgrade
    git-svn-id: svn://svn.code.sf.net/p/puttysm/code/trunk@96 024b630a-0915-0410-a176-cfe2cd84e366

--- PuTTYSessionManager/Properties/Settings.Designer.cs
+++ PuTTYSessionManager/Properties/Settings.Designer.cs
@@ -358,5 +358,17 @@ namespace uk.org.riseley.puttySessionManager.Properties {
                 this["SortOrder"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
